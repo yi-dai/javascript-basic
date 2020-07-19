dd0e5fae14127d240cf99c20806a1158802fe073
yi dai <daiyi@yideMacBook-Pro.local>
2020-07-20 04:33:26 +0800

    [yi-dai] fit: event_spec.js exercise.

--- __tests__/pieceOfCake/event_spec.js
+++ __tests__/pieceOfCake/event_spec.js
@@ -16,6 +16,7 @@ describe('for event', () => {
 
     // <--start
     // Please add the event listener to handle `click` event on `element`.
+    element.addListener('click', () => onClick());
 
     // --end->
 
@@ -34,7 +35,7 @@ describe('for event', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = ['I have been clicked', 'I have been clicked'];
         // --end->
 
         expect(logs).toEqual(expected);
@@ -57,7 +58,7 @@ describe('for event', () => {
       .then(() => {
         // <--start
         // Please write down the correct value. You should write the final result directly.
-        const expected = undefined;
+        const expected = ['I have been clicked'];
         // --end->
 
         expect(logs).toEqual(expected);
